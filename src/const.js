a5aaf19f03660a03b9a83ae2703b5a483d48741e
Andrey Filippov <androsphilippos@gmail.com>
2021-08-23 10:34:44 +0500

    вынес часть данных в const

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const SITE_MENU_LIST = ['Автомобили', 'Контакты', 'Услуги', 'Вакансии'];
+
+export const FOOTER_SITE_SECTIONS = ['Корпоративным клиентам', 'Клиентам', 'Аренда авто', 'Как продать авто', 'Trade-in', 'Test drive'];
+
+export const TabsName = {
+  SPECIFICATIONS: 'Характеристики',
+  REVIEWS: 'Отзывы',
+  CONTACTS: 'Контакты',
+};
+
+export const STARS_VALUES = ['5', '4', '3', '2', '1'];
