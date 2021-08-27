f8c2f14daba51e3447f6f4e7ed3865354996de3b
Andrey Filippov <androsphilippos@gmail.com>
2021-08-27 09:11:37 +0500

    добавил в const.js: FOOTER_SITE_SECTIONS недостающий пункт - Каршеринг

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 export const SITE_MENU_LIST = ['Автомобили', 'Контакты', 'Услуги', 'Вакансии'];
 
-export const FOOTER_SITE_SECTIONS = ['Корпоративным клиентам', 'Клиентам', 'Аренда авто', 'Как продать авто', 'Trade-in', 'Test drive'];
+export const FOOTER_SITE_SECTIONS = ['Корпоративным клиентам', 'Клиентам', 'Аренда авто', 'Каршеринг', 'Как продать авто', 'Trade-in', 'Test drive'];
 
 export const TabsName = {
   SPECIFICATIONS: 'Характеристики',
